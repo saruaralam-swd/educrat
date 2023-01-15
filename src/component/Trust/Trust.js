da3f2767bcddb74ea248138300fa0b54b0229b4e
saruaralam-swd <saruaralam2001@gmail.com>
2023-01-15 20:58:52 +0600

    tust company section redesign

--- src/component/Trust/Trust.js
+++ src/component/Trust/Trust.js
@@ -1,23 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import SingleTrust from '../SingleTruset/SingleTrust';
 
 const Trust = () => {
-  const [imgs, setImgs]  = useState([]);
+  const [imgs, setImgs] = useState([]);
 
   useEffect(() => {
     fetch('images.json')
-    .then (res => res.json())
-    .then (data => setImgs(data))
+      .then(res => res.json())
+      .then(data => setImgs(data))
   }, []);
 
   return (
-    <div className='mt-12'>
-      <h2 className='mb-5 text-center'>Trusted by the world’s best</h2>
-      <div className='flex justify-between'>
-      {
-        imgs.map(img => <SingleTrust key={img.id} img={img}></SingleTrust>)
-      }
-    </div>
+    <div className='mt-12 px-5 md:px-10'>
+      <h2 className='text-center text-lg'>Trusted by the world’s best</h2>
+      <div className='flex justify-between mt-5 gap-5'>
+        {
+          imgs.map(img => <div key={img.id}><img src={img.picture} alt="" /></div>)
+        }
+      </div>
     </div>
   );
 };
